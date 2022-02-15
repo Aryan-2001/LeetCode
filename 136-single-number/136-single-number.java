4d7b75ad808ebf646a48a8c469d148f320452c0a
ARYAN GUPTA <monaaru2001@gmail.com>
2022-02-15 14:29:35 +0530

    Time: 2 ms (64.57%), Space: 50.1 MB (35.27%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int t = 0;
+        
+        for(int i:nums){
+            t = t^i;
+        }
+        
+        return(t);
+        
+    }
+}
