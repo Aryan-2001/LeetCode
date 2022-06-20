f441ccb610ad37e4d36e155f24f9db5549446f92
ARYAN GUPTA <monaaru2001@gmail.com>
2022-06-20 18:06:04 +0530

    Time: 382 ms (8.54%), Space: 15.3 MB (10.57%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -0,0 +1,42 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        
+        sm = 0 
+        
+        mt = []
+        l = 0
+        for i in nums:
+            sm+=i
+            mt.append(sm)
+            l+=1
+        
+        #print(mt)
+        temp = 0
+        
+        for i in mt:
+            
+            if(temp==0):
+                
+                left = 0
+                right = mt[-1] - i
+            
+            elif(temp==l-1):
+                
+                left = mt[-2]
+                right = 0
+            
+            else:
+                
+                left = mt[temp-1]
+                right = mt[-1] - mt[temp]
+                
+            temp+=1
+            print(left,right)
+            if(left==right):
+                return temp-1
+            
+        return -1
+            
+            
+        
